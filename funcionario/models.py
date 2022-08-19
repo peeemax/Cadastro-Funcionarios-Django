da0ed71fe32772ce453ca9ec72b9fa6a996a9866
peeemax <peeemax@hotmail.com>
2022-08-18 21:49:51 -0300

    Colcada funções do CRUD dos contatos novos para cada funcionario

--- funcionario/models.py
+++ funcionario/models.py
@@ -1,8 +1,9 @@
+import email
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Funcionario(models.Model):
-    objects = None
     nome_completo = models.CharField(max_length=256)
     cpf = models.IntegerField()
     data_nascimento = models.DateField(null=True)
@@ -12,3 +13,12 @@ class Funcionario(models.Model):
     def __str__(self) -> str:
         return self.nome_completo
 
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=256)
+    email = models.EmailField(max_length=256)
+    telefone = models.CharField(max_length=20)
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.nome
