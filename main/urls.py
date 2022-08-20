df49ad72746a00757cfac691e6afd7b5646438b2
peeemax <peeemax@hotmail.com>
2022-08-20 17:01:00 -0300

    Realizado a tela para cadastro de usuario

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,8 @@
-from atexit import register
-from django import views
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, register
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', login_required(HomeView.as_view()), name='home'),
+    path('accounts/register', register, name='register')
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from multiprocessing import context
+from django.contrib import messages
+from django.contrib.auth import login
+
+from main.forms import NovoUsuarioForm
 
 
 class HomeView(TemplateView):
     template_name = 'main/index.html'
+
+def register(request):
+    if request.method == "POST":
+        form = NovoUsuarioForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Cadastro realizado com sucesso!")
+            return redirect('home')
+        messages.error(request, "Falha no cadastro de usuário")
+    form = NovoUsuarioForm()
+    context = {'form': form}
+    return render(request, template_name='main/register.html', context=context)
